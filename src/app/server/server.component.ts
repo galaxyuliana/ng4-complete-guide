3d030a3c55745f7478c955e697d412931f0d7eeb
galaxyuliana <yulianashin613@gmail.com>
2022-08-05 23:52:52 -0700

    Added server component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'
+
+// Using decorator to tell
+// This is not only a class
+// decorator uses @
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+
+// export class to use it from outside
+export class ServerComponent {
+
+}
+// component is a TypeScript class
