798c176de1e1eab6041b464271a2e6dd47ad4575
galaxyuliana <yulianashin613@gmail.com>
2022-08-06 23:46:13 -0700

    Added servers component with CLI

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+// component can be added by using CLI
+// ng generate component NAME_OF_COMPONENT
+// ng g c NAME_OF_COMPONENT
