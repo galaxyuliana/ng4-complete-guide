0b6f6bd75f62c33cdb2fd288bf2e00a3675defff
galaxyuliana <yulianashin613@gmail.com>
2022-08-07 23:44:49 -0700

    Added component selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selecting by ID, pseudo-selectors are not supported by Angular
   selector: 'app-servers',
+  // selector: '[app-servers]', // change selector to be an attribute
+  // selector: '.app-servers', // change selector to be an class
   // every component must have template or templateUrl
   // Using external HTML file would be better if you need more than 3 lines of HTML code
   template: `
